e475f00b4f0343deaac1fb57c58d8e824381994f
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-11-28 11:03:34 -0800

    update TripAPI info with more detailed description

--- src/Components/Trip/TripAPI.js
+++ src/Components/Trip/TripAPI.js
@@ -12,7 +12,7 @@ const TripItem = [
   {
     image: "./images/maldives.webp",
     title: "Trip in Maldives",
-    desc: "Treat yourself well with a beautiful boat trip on the sea! Leave all your cares behind on a boat trip with the sailing boat, yacht or speed boat, enjoy a relaxing day on a catamaran or observe the gorgeous underwater world from a glas-bottom boat. On a boat trip you can discover the impressive coast of Athens",
+    desc: "Treat yourself with a beautiful boat trip on the sea! Leave all your cares behind on a boat trip with the sailing boat, yacht or speed boat, enjoy a relaxing day on a catamaran or observe the gorgeous underwater world from a glas-bottom boat. On a boat trip you can discover the impressive coast of Athens",
   },
 ];
 export default TripItem;
