f7b52bd941967aaf0e0c831a3723c7ff7239a5b3
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-12-27 09:11:40 -0800

    update description for Trip to Japan

--- src/Components/Trip/TripAPI.js
+++ src/Components/Trip/TripAPI.js
@@ -7,7 +7,7 @@ const TripItem = [
   {
     image: "./images/japan_street.webp",
     title: "Trip in Japan",
-    desc: "Treat yourself well with a beautiful boat trip on the sea! Leave all your cares behind on a boat trip with the sailing boat, yacht or speed boat, enjoy a relaxing day on a catamaran or observe the gorgeous underwater world from a glas-bottom boat. On a boat trip you can discover the impressive coast of Athens",
+    desc: "Take an adventure in Japan and see the beatiful history and culture this country has to offer!",
   },
   {
     image: "./images/maldives.webp",
